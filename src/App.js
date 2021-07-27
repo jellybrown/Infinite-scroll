dd9550b2036fbc51954694a639b98ec0e2a6eac8
yujin mac <kimyu28jin@gmail.com>
2021-07-27 13:31:32 +0900

    Feat : Infinite scroll 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,72 @@
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Comments from "./components/Comments";
+import axios from "axios";
 
 function App() {
+  const [page, setPage] = useState(1);
+  const [list, setList] = useState(null);
+  const [element, setElement] = useState(null);
+
+  const fetchData = async (page) => {
+    const result = await axios.get(
+      `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=10`
+    );
+    setList((prev) => {
+      if (!prev) return result.data;
+      else return [...prev, ...result.data];
+    });
+  };
+
+  const observer = useRef(
+    new IntersectionObserver(
+      (entries) => {
+        const first = entries[0];
+        console.log(first);
+        if (first.isIntersecting) {
+          setPage((prev) => prev + 1);
+        }
+      },
+      { threshold: 1 }
+    )
+  );
+
+  useEffect(() => {
+    fetchData(page);
+  }, [page]);
+
+  useEffect(() => {
+    const currentElement = element;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
+    };
+  }, [element]);
+
   return (
     <div>
-      <Comments />
+      <div>
+        {list &&
+          list.map((comment, index) => {
+            return (
+              <div ref={setElement} key={index}>
+                <Comments
+                  postId={comment.postId}
+                  email={comment.email}
+                  name={comment.name}
+                  body={comment.body}
+                />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,24 +1,20 @@
 import React from "react";
 import { CommentWrapper, RowInfo, Title } from "./Comments.style";
 
-const Comments = () => {
+const Comments = ({ postId, email, name, body }) => {
   return (
     <CommentWrapper>
       <RowInfo>
         <Title>Comment Id</Title>
-        <span>3</span>
+        <span>{postId}</span>
       </RowInfo>
       <RowInfo>
         <Title>Email</Title>
-        <span>3gdsd@na.aa</span>
+        <span>{email}</span>
       </RowInfo>
       <RowInfo>
         <Title>Comment</Title>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-          dolorum animi commodi maxime omnis repellat! Similique quo unde
-          voluptatibus doloribus?
-        </p>
+        <p>{body}</p>
       </RowInfo>
     </CommentWrapper>
   );
